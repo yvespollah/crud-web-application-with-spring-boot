4f774300492f5b6344dbb8f9f47c9743eda2d228
pollah yves <yvesfranky961@gmail.com>
2024-07-18 16:33:21 +0100

    start web application crud with spring boot

---
+++ src/main/java/com/pollahyves/webappcrud/WebappcrudApplication.java
@@ -0,0 +1,13 @@
+package com.pollahyves.webappcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pollahyves/webappcrud/WebappcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollahyves.webappcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
